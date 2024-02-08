c783a37ac34576561ed51c7220eec66d2f5dff7a
mattpe <mattpe@metropolia.fi>
2024-02-08 11:43:46 +0200

    user resource endpoints done, (not fully working yet)

--- src/controllers/user-controller.mjs
+++ src/controllers/user-controller.mjs
@@ -1,4 +1,10 @@
-import {listAllUsers, selectUserById} from "../models/user-model.mjs";
+import {
+  deleteUserById,
+  insertUser,
+  listAllUsers,
+  selectUserById,
+  updateUserById,
+} from '../models/user-model.mjs';
 
 // TODO: implement route handlers below for users (real data)
 
@@ -18,14 +24,41 @@ const getUserById = async (req, res) => {
   return res.json(result);
 };
 
-const postUser = (req, res) => {
-  // TODO: implement this
-  res.send('not working yet');
+const postUser = async (req, res) => {
+  const {username, password, email} = req.body;
+  // check that all needed fields are included in request
+  if (username && password && email) {
+    const result = await insertUser(req.body);
+    if (result.error) {
+      return res.status(result.error).json(result);
+    }
+    return res.status(201).json(result);
+  } else {
+    return res.status(400).json({error: 400, message: 'bad request'});
+  }
 };
 
-const putUser = (req, res) => {
-  // TODO: implement this
-  res.send('not working yet');
+const putUser = async (req, res) => {
+  const user_id = req.params.id;
+  const {username, password, email} = req.body;
+  // check that all needed fields are included in request
+  if (user_id && username && password && email) {
+    const result = await updateUserById({user_id, ...req.body});
+    if (result.error) {
+      return res.status(result.error).json(result);
+    }
+    return res.status(201).json(result);
+  } else {
+    return res.status(400).json({error: 400, message: 'bad request'});
+  }
+};
+
+const deleteUser = async (req, res) => {
+  const result = await deleteUserById(req.params.id);
+  if (result.error) {
+    return res.status(result.error).json(result);
+  }
+  return res.json(result);
 };
 
 // Dummy login with mock data, returns user object if username & password match
@@ -34,7 +67,7 @@ const postLogin = (req, res) => {
   if (!userCreds.username || !userCreds.password) {
     return res.sendStatus(400);
   }
-  const userFound = users.find(user => user.username == userCreds.username);
+  const userFound = users.find((user) => user.username == userCreds.username);
   // user not found
   if (!userFound) {
     return res.status(403).json({error: 'username/password invalid'});
@@ -47,4 +80,4 @@ const postLogin = (req, res) => {
   }
 };
 
-export {getUsers, getUserById, postUser, putUser, postLogin};
+export {getUsers, getUserById, postUser, putUser, postLogin, deleteUser};
--- src/models/user-model.mjs
+++ src/models/user-model.mjs
@@ -26,9 +26,55 @@ const selectUserById = async (id) => {
     delete rows[0].password;
     return rows[0];
   } catch (error) {
-    console.error('listAllUsers', error);
+    console.error('selectUserById', error);
     return {error: 500, message: 'db error'};
   }
 };
 
-export {listAllUsers, selectUserById};
+const insertUser = async (user) => {
+  try {
+    const sql = 'INSERT INTO Users (username, password, email) VALUES (?, ?, ?)';
+    const params = [user.username, user.password, user.email];
+    const [result] = await promisePool.query(sql, params);
+    //console.log(result);
+    return {message: 'new user created', user_id: result.insertId};
+  } catch (error) {
+    // now duplicate entry error is generic 500 error, should be fixed to 400 ?
+    console.error('insertUser', error);
+    return {error: 500, message: 'db error'};
+  }
+};
+
+const updateUserById = async (user) => {
+  try {
+    const sql = 'UPDATE Users SET username=?, password=?, email=? WHERE user_id=?';
+    const params = [user.username, user.password, user.email, user.user_id];
+    const [result] = await promisePool.query(sql, params);
+    console.log(result);
+    return {message: 'user data updated', user_id: user.user_id};
+  } catch (error) {
+    // fix error handling
+    // now duplicate entry error is generic 500 error, should be fixed to 400 ?
+    console.error('updateUserById', error);
+    return {error: 500, message: 'db error'};
+  }
+};
+
+const deleteUserById = async (id) => {
+  try {
+    const sql = 'DELETE FROM Users WHERE user_id=?';
+    const params = [id];
+    const [result] = await promisePool.query(sql, params);
+    console.log(result);
+    if (result.affectedRows === 0) {
+      return {error: 404, message: 'user not found'};
+    }
+    return {message: 'user deleted', user_id: id};
+  } catch (error) {
+    // note that users with other data (FK constraint) cant be deleted directly
+    console.error('deleteUserById', error);
+    return {error: 500, message: 'db error'};
+  }
+};
+
+export {listAllUsers, selectUserById, insertUser, updateUserById, deleteUserById};
